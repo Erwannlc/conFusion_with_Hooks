584277374af97c0785634b1c260b2f46e1afbd41
Erwann Le Corre <lcerwann@gmail.com>
2022-01-01 14:02:29 +0100

    Fetch Handling Errors

--- src/redux/ActionsCreators.js
+++ src/redux/ActionsCreators.js
@@ -13,9 +13,27 @@ export const addComment = (dishId, rating, author, comment) => ({
 
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
+
     return fetch(baseUrl + 'dishes')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            }
+            else {
+                 let error = new Error('Error' + response.status + ': ' + response.statusText)
+                 error.response = response;
+                 throw error;
+
+            }
+        }, 
+        /* Quand on ne reçoit aucune info du serveur, qd le serveur ne rpéond pas du tout : */
+        error => {
+            let errmess = new Error(error.message);
+            throw errmess;
+        })
         .then(response => response.json())
-        .then(dishes => dispatch(addDishes(dishes)));
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)))
 }
 
 export const dishesLoading = () => ({
@@ -35,8 +53,25 @@ export const addDishes = (dishes) => ({
 
 export const fetchComments = () => (dispatch) => {
         return fetch(baseUrl + 'comments')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            }
+            else {
+                 let error = new Error('Error' + response.status + ': ' + response.statusText)
+                 error.response = response;
+                 throw error;
+
+            }
+        }, 
+        /* Quand on ne reçoit aucune info du serveur, qd le serveur ne rpéond pas du tout : */
+        error => {
+            let errmess = new Error(error.message);
+            throw errmess;
+        })
         .then(response => response.json())
-        .then(comments => dispatch(addComments(comments)));
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)))
 }
 
 export const commentsFailed = (errmess) => ({
@@ -55,8 +90,25 @@ export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading());
 
         return fetch(baseUrl + 'promotions')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            }
+            else {
+                 let error = new Error('Error' + response.status + ': ' + response.statusText)
+                 error.response = response;
+                 throw error;
+
+            }
+        }, 
+        /* Quand on ne reçoit aucune info du serveur, qd le serveur ne rpéond pas du tout : */
+        error => {
+            let errmess = new Error(error.message);
+            throw errmess;
+        })
         .then(response => response.json())
-        .then(promos => dispatch(addPromos(promos)));
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)))
 }
 
 export const promosLoading = () => ({
